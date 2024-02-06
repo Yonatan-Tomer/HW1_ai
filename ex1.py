34544994ac3069d56a89d60fe179015edbd0bf42
tomerkatz <tomerkatz37@gmail.com>
2024-02-06 22:39:27 +0200

    started

--- ex1.py
+++ ex1.py
@@ -3,7 +3,7 @@ import random
 import math
 
 
-ids = ["111111111", "111111111"]
+ids = ["325028967", "213125164"]
 
 
 class OnePieceProblem(search.Problem):
@@ -13,12 +13,32 @@ class OnePieceProblem(search.Problem):
         """Don't forget to implement the goal test
         You should change the initial to your own representation.
         search.Problem.__init__(self, initial) creates the root node"""
+        for i, row in enumerate(self.map):  # find base location
+            for j, loc in enumerate(row):
+                if loc == "B":
+                    self.base = tuple(i, j)
+        self.treasure_in_base = []
+        self.map = initial["map"]  # map as is in initial
+        #self.pirate_ships = {}  # list of all ships and treasures they have onboard
+        #for ship in initial["pirate_ships"]:
+            #self.pirateShips[ship] = {"location": initial["pirate_ships"][ship], "treasures": []}
+        self.treasures = initial["treasures"]  # treasures as is in initial
+        self.marine_ships = initial["marine_ships"].values()
         search.Problem.__init__(self, initial)
         
     def actions(self, state):
         """Returns all the actions that can be executed in the given
         state. The result should be a tuple (or other iterable) of actions
         as defined in the problem description file"""
+        # state = [ships, treasures, marines]
+        actions = []
+        # sail
+
+        # collect
+
+        # deposit
+
+        # wait
 
     def result(self, state, action):
         """Return the state that results from executing the given
@@ -37,6 +57,45 @@ class OnePieceProblem(search.Problem):
 
     """Feel free to add your own functions
     (-2, -2, None) means there was a timeout"""
+
+    def sail_actions(self, ships):#todo complete
+
+        for ship in ships:
+            loc = ship["location"]
+            if not loc[0] == 0:  #up
+                if self.map[loc[0]][loc[1]-1] == 'S':
+                    yield
+            if not loc[0] == len(self.map)-1:  # down
+                if self.map[loc[0]][loc[1]+1] == 'S':
+                    yield
+            if not loc[1] == 0:  # left
+                if self.map[loc[0]-1][loc[1]] == 'S':
+                    yield
+            if not loc[1] == len(self.map[0])-1:  #right
+                if self.map[loc[0]+1][loc[1]] == 'S':
+
+    def collect_actions(self, ships):
+        for ship in ships:
+            for treasure in self.treasures:
+                if abs(ship["location"][0] - self.treasures[treasure][0]) == 1 and\
+                    ship["location"][1] - self.treasures[treasure][1] == 0:
+                    yield
+                if abs(ship["location"][1] - self.treasures[treasure][1]) == 1 and\
+                    ship["location"][0] - self.treasures[treasure][0] == 0:
+                    yield
+
+
+    def deposit_actions(self, ships):
+        for ship in ships:
+            if ship["location"][0] - self.base[0] == 0 and \
+                    ship["location"][1] - self.base[1] == 0:
+                yield
+
+
+
+
+
+
 
 
 def create_onepiece_problem(game):
